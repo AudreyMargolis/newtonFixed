1e2236771fde4a98b36e2a8dc720ff913c126a21
Erith Margolis <dischordiandesigns@gmail.com>
2017-01-15 12:06:40 -0800

    fan and bounce tile stages made

---
+++ testlol/Assets/Scripts/TutPaddle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutPaddle : MonoBehaviour
+{
+    public enum paddleStates {TUTOPEN, ROTONLY, HITONLY};
+    public paddleStates state;
+   
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	  if(state != paddleStates.TUTOPEN)
+        {
+            if(state != paddleStates.ROTONLY)
+            {
+                Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+                Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+                lookPos = lookPos - transform.position;
+                float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+            if(state != paddleStates.HITONLY)
+            {
+
+            }
+        }
+
+	}
+}
---
+++ testlol/Assets/Scripts/WindTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindTrigger : MonoBehaviour
+{
+    public enum WindDirection {UP,DOWN,LEFT,RIGHT};
+    public WindDirection windDir;
+    public float windForce;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            if (windDir == WindDirection.UP)
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * windForce);
+            if (windDir == WindDirection.LEFT)
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0) * windForce);
+            if (windDir == WindDirection.RIGHT)
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, 0) * windForce);
+            if (windDir == WindDirection.DOWN)
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, -1, 0) * windForce);
+        }
+    }
+}
